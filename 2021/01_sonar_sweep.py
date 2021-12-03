3787557f2e5c9ab12d1929040b67d1c84c048033
Joseph Cosentino <Joseph_Cosentino@comcast.com>
2021-12-03 16:35:09 -0500

    formatting

--- 2021/01_sonar_sweep.py
+++ 2021/01_sonar_sweep.py
@@ -2,8 +2,10 @@
 from typing import List
 from util import get_input
 
+
 def sonar_sweep(input: List[int], window: int = 1) -> int:
     return sum(curr > prev for prev, curr in zip(input, input[window:]))
+
 
 if __name__ == '__main__':
     input = [int(n) for n in get_input(year=2021, day=1)]
